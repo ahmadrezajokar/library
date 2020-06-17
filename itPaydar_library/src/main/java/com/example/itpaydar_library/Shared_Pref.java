8efa81e1c2f837ac402fe1dfe8a3e63d62e3280d
ehsan.karimi <21437080c>
2020-06-17 11:14:59 +0430

    Initial commit

---
+++ itPaydar_library/src/main/java/com/example/itpaydar_library/Shared_Pref.java
@@ -0,0 +1,11 @@
+package com.example.itpaydar_library;
+
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Toast;
+
+public class Shared_Pref extends Activity {
+    public void toast(Context context){
+        Toast.makeText(context,"hello",Toast.LENGTH_LONG).show();
+    }
+}
